d37ba8d747e26fcf9fc0782238582e5a7812de1c
Alberto Herrera Vargas <albertoherreravargas@gmail.com>
2021-04-18 11:29:29 +0200

    Starting with the first test. Installing required dev dependencies

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Loot Check</h2>
+            </div>
+        )
+    }
+}
